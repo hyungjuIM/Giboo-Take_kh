1d5a2f29238b2d47411fc765f4b75ed9c577600a
chan <unable.video0w@icloud.com>
2023-05-22 21:28:06 +0900

    프로젝트 파일  초안

---
+++ back/comm/src/main/java/com/kh/fin/main/controller/MainController.java
@@ -0,0 +1,22 @@
+package com.kh.fin.main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	
+	/*
+	 * http://localhost:8080/comm/main
+	 * http://localhost:8080/comm/
+	 * 
+	 * */
+	
+	
+	@RequestMapping("/main")
+	public String mainForward() {
+		
+		return "common/main";
+		
+	}
+}
