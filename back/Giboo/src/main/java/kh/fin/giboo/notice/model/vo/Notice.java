bfabcb94c78af3df9770807a8fe5fbc755914334
IM_hyungju <125863770+hyungjuIM@users.noreply.github.com>
2023-05-25 18:06:28 +0900

    notice front

---
+++ back/Giboo/src/main/java/kh/fin/giboo/notice/model/vo/Notice.java
@@ -0,0 +1,21 @@
+package kh.fin.giboo.notice.model.vo;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Notice {
+	private int noticeNo;
+	private String noticeTitle;
+	private String noticeContent;
+	private String ncreateDT;
+	private String nupdateDT;
+	private int noticeCount;
+	private String ndeleteStatus;
+	
+	private int mgrNo;
+}
