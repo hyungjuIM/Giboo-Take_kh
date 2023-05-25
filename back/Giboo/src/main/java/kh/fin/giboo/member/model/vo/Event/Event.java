7df27ddc46677796f02442e40b42fa2ca496d14f
diaperi <125863682+diaperi@users.noreply.github.com>
2023-05-25 14:40:47 +0900

    이벤트, 즐겨찾기 vo (#19)

---
+++ back/Giboo/src/main/java/kh/fin/giboo/member/model/vo/Event/Event.java
@@ -0,0 +1,21 @@
+package kh.fin.giboo.member.model.vo.Event;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Event {
+	
+	private int eventNo;
+	private String eventTitle;
+	private String eventContent;
+	private String enrollDt;
+	private String modiDt;
+	private int eventViews;
+	private String deleteStatus;
+	private String eventImg;
+	private int mrgNo;
+}
