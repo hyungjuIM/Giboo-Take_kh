e5fc2e19fdd71a851b869442f4413cda19c0f529
seulki <seulki901234@gmail.com>
2023-06-07 10:17:07 +0900

    mypage

--- front/JS/alarm.js
+++ front/JS/alarm.js
@@ -1,9 +1,7 @@
 // 알림 버튼 클릭 시 알림 팝업 토글
 $('.tooltip[data-text="알림"]').on('click', function() {
-  $('.alarmPopup').toggleClass('show');
-});
-
-$('.tooltip[data-text="알림2"]').on('click', function() {
   $('.alert_container').toggleClass('show');
 });
+
+
 
--- front/JS/mypage/alert1.js
+++ front/JS/mypage/alert1.js
@@ -1,6 +1,4 @@
 // 알림 버튼 클릭 시 알림 팝업 토글
 $('.tooltip[data-text="알림"]').on('click', function() {
-    $('.alarmPopup').toggleClass('show');
-  });
-  
-  
+  $('.alert_container').toggleClass('show');
+});
