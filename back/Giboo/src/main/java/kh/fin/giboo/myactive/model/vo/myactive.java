7f579d42be57c0930cfc44fa59e71a2d18495f54
LAPTOP-UHOL95SQ\leana <diaperi0901@gmail.com>
2023-06-09 20:02:03 +0900

    푸시 위한 커밋, 맴 수정

---
+++ back/Giboo/src/main/java/kh/fin/giboo/myactive/model/vo/myactive.java
@@ -0,0 +1,21 @@
+package kh.fin.giboo.myactive.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+public class myactive {
+
+	//나의 활동
+	private int myactNo;
+	private String volcomStatus;
+	private int donationNo;
+	private int volunteerNo;
+	private int memberNo;
+	private int eventNo;
+}
