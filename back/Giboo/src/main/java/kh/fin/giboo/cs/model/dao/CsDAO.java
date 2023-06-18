95c625613193bae77672e9248641b72176e7e51b
DESKTOP-PM61EPR\parksangbin <zaza1599@gmail.com>
2023-06-18 19:33:56 +0900

    Cs

---
+++ back/Giboo/src/main/java/kh/fin/giboo/cs/model/dao/CsDAO.java
@@ -0,0 +1,10 @@
+package kh.fin.giboo.cs.model.dao;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CsDAO {
+	private Logger logger = LoggerFactory.getLogger(CsDAO.class);
+}
