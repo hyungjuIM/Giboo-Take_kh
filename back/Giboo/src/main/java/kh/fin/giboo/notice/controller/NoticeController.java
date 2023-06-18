0272754cdf08ba6e6686582600627e16fdcbcd99
IM_hyungju <125863770+hyungjuIM@users.noreply.github.com>
2023-06-18 18:44:22 +0900

    noticeList

--- back/Giboo/src/main/java/kh/fin/giboo/notice/controller/NoticeController.java
+++ back/Giboo/src/main/java/kh/fin/giboo/notice/controller/NoticeController.java
@@ -1,23 +1,144 @@
 package kh.fin.giboo.notice.controller;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import kh.fin.giboo.member.model.vo.Member;
+import kh.fin.giboo.notice.model.service.NoticeService;
+import kh.fin.giboo.notice.model.vo.NoticeDetail;
 
 @Controller
 @RequestMapping("/notice")
 public class NoticeController {
 	private Logger logger = LoggerFactory.getLogger(NoticeController.class);
 	
+	@Autowired
+	private NoticeService service;
 	
 	// 공지사항 리스트
 	@GetMapping(value="/noticeList")
-	public String noticeList() {
+		public String noticeList(
+				@RequestParam(value = "cp", required = false, defaultValue = "1") int cp,
+			Model model
+			) {
+		
+		Map<String, Object> map = null;
+
+		map = service.selectNoticeList(cp, model);
+
+		model.addAttribute("map",map);
 		logger.info("공지사항");
+
 		return "notice/noticeList";
 	}
+	
+	
+	// 게시글 상세 조회
+	@GetMapping(value="/noticeDetail/{noticeNo}")
+	public String noticeDetail(
+			@PathVariable("noticeNo") int noticeNo,
+			@RequestParam(value = "cp", required = false, defaultValue = "1") int cp,
+			Model model,
+			HttpSession session,
+			HttpServletRequest req,HttpServletResponse resp ) {
+
+		logger.info("공지사항 상세조회");
+		// 게시글 상세 조회 서비스 호출
+		NoticeDetail noticeDetail = service.selectNoticeDetail(noticeNo);
+
+
+
+		if( noticeDetail != null ) { // 상세 조회 성공 시
+			
+		
+			
+
+			Member loginMember = (Member)session.getAttribute("loginMember");
+
+			int memberNo = 0;
+
+			if(loginMember != null) {
+				memberNo = loginMember.getMemberNo();
+			}
+
+			// 글쓴이와 현재 클라이언트(로그인 한 사람)가 같지 않은 경우 -> 조회 수 증가
+			if( noticeDetail.getMemberNo() != memberNo ) {
+
+				Cookie cookie = null; // 기존에 존재하던 쿠키를 저장하는 변수
+
+				Cookie[] cArr = req.getCookies(); // 쿠키 얻어오기 
+
+				if(cArr != null && cArr.length > 0) { // 얻어온 쿠키가 있을 경우
+
+					for(Cookie c : cArr) {
+
+						if(c.getName().equals("readNoticeNo")) {
+							cookie = c;
+						}
+
+					}
+				}
+
+
+
+				int result = 0;
+
+
+
+				if ( cookie == null ) { // 기존에 "readnoticeNo" 이름의 쿠키가 없던 경우
+					cookie = new Cookie("readNoticeNo", noticeNo+"");
+					result = service.updateViewCount(noticeNo);
+
+				} else {
+					String[] temp = cookie.getValue().split("/");
+
+
+					List<String> list = Arrays.asList(temp); // 배열 -> List 변환
+
+
+					if( list.indexOf(noticeNo+"") == -1 ) { // 기존 값에 같은 글번호가 없다면 추가
+
+						cookie.setValue( cookie.getValue() + "/" + noticeNo );
+						result = service.updateViewCount(noticeNo); // 조회수 증가 서비스 호출
+
+					}
+				}
+
+
+				// 결과값 이용한 DB 동기화
+				if (result > 0) {
+					noticeDetail.setViewCount(noticeDetail.getViewCount() + 1); // 이미 조회된 데이터 DB 동기화
+
+					cookie.setPath(req.getContextPath());
+					cookie.setMaxAge(60 * 60 * 1);
+					resp.addCookie(cookie);
+
+				}
+			}
+		}
+
+		model.addAttribute("noticeDetail", noticeDetail);
+		return "notice/noticeDetail";
+
+	}
+	
 	
 	
 	// 공지사항 작성
--- back/Giboo/src/main/java/kh/fin/giboo/notice/model/service/NoticeService.java
+++ back/Giboo/src/main/java/kh/fin/giboo/notice/model/service/NoticeService.java
@@ -1,5 +1,18 @@
 package kh.fin.giboo.notice.model.service;
 
+import java.util.Map;
+
+import org.springframework.ui.Model;
+
+import kh.fin.giboo.notice.model.vo.NoticeDetail;
+
 public interface NoticeService {
+
+	Map<String, Object> selectNoticeList(int cp, Model model);
+
+	NoticeDetail selectNoticeDetail(int noticeNo);
+
+	int updateViewCount(int noticeNo);
+
 
 }
